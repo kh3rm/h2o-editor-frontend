7e76c3ffaae39a5b6264bdaac1e92dcf1760395f
kh3rm <h3rmanoo@gmail.com>
2025-09-14 14:40:52 +0200

    Edit App.jsx, add Header/Footer view-components

---
+++ src/view-components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+
+    return (
+      <>
+        <footer>
+            <p class="copyright">&copy; H(2)O Document Editor</p>
+        </footer>
+      </>
+    );
+  }
+
+  export default Footer;
---
+++ src/view-components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+
+    return (
+      <>
+        <header>
+            <h1><a href="/">H(2)O Document Editor</a></h1>
+        </header>
+      </>
+    );
+  }
+
+  export default Header;
