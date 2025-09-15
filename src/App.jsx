f959e30201d828e27a2ebe0cb994fd9c42fea898
kh3rm <h3rmanoo@gmail.com>
2025-09-15 18:31:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './view-components/Header'
+import Main from './view-components/Main'
 import Footer from './view-components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header/>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <Footer/>
+    <Header/>
+
+    <Main/>
+
+    <Footer/>
     </>
   )
 }
