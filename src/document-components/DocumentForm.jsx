73e46e407e98a9eb05fe4bc7ba58e32ce3cfb653
h3rmano <h3rmanoo@gmail.com>
2025-10-06 06:36:06 +0200

    Edit DocumentForm.jsx

--- src/document-components/DocumentForm.jsx
+++ src/document-components/DocumentForm.jsx
@@ -1,48 +1,39 @@
-import React from 'react';
-import { useDocumentContext } from './DocumentContext';
+import React from "react";
+import { useDocumentContext } from "./DocumentContext";
 
 /**
  * @component DocumentForm
  * Handles the loading of the document-form, for document creation/update.
  */
 function DocumentForm() {
-    const {
-        title,
-        setTitle,
-        content,
-        setContent,
-        createDocument,
-        updateDocument,
-        updateId,
-        mode
-    } = useDocumentContext();
+  const {
+    title,
+    content,
+    socketTitleChange,
+    socketContentChange,
+  } = useDocumentContext();
 
-    return (
-        <div className="document-form">
-            <label htmlFor="title">Title</label>
-            <input
-                type="text"
-                id="title"
-                name="title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
+  return (
+    <div className="document-form">
+      <label htmlFor="title">Title</label>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={title}
+        onChange={(e) => socketTitleChange(e.target.value)}
+      />
 
-            <label htmlFor="content">Content</label>
-            <textarea
-                id="content"
-                name="content"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-            ></textarea>
-
-            <button className="document-form-button" onClick={updateId === null ? createDocument : updateDocument}>
-                {updateId === null ? "Create New Document" : "Update Document"}
-            </button>
-
-            <br />
-        </div>
-    );
+      <label htmlFor="content">Content</label>
+      <textarea
+        id="content"
+        name="content"
+        value={content}
+        onChange={(e) => socketContentChange(e.target.value)}
+      ></textarea>
+      <br />
+    </div>
+  );
 }
 
 export default DocumentForm;
