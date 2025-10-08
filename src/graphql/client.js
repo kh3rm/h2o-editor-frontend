efbf455a7641a6d902f9b09b44858bf9ae0848a7
lohengrin1337 <149483141+lohengrin1337@users.noreply.github.com>
2025-10-08 10:37:54 +0200

    Adjust graphQLClient - same for query and mutation

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -9,11 +9,11 @@ const H2O_GRAPHQL_API_URI = 'http://localhost:3000/graphql';
  */
 export const graphQLClient = {
     /**
-     * Send Query to a graphQL endpoint
+     * Send Query or Mutation to a graphQL endpoint
      * 
      * @async
-     * @param {string} query an SDL string
-     * @param {Object} variables for query arguments (optional)
+     * @param {string} query an SDL string (query or mutation)
+     * @param {Object} variables for query/mutation arguments (optional)
      * 
      */
     async query(query, variables = null) {
@@ -30,8 +30,4 @@ export const graphQLClient = {
             body: JSON.stringify(payload)
         });
     },
-
-    async mutation() {
-        // TODO
-    }
 };
