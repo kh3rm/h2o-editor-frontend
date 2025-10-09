88f86998031175c90008b613872de042aae1167d
h3rmano <h3rmanoo@gmail.com>
2025-10-09 16:00:15 +0200

    Modify header with experimental shorter, tidier app-name: docpool

--- src/view-components/Header.jsx
+++ src/view-components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <header>
       <div className="header-logo">
         <img src={logo} alt="Logo" style={{ width: 49, height: 49 }} />
-        <h1 className="header-title">Document Editor</h1>
+        <h1 className="header-title">docpool</h1>
       </div>
     </header>
   );
