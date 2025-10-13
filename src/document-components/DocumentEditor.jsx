8358dabd1796053927c40359473da2f6f08256aa
h3rmano <h3rmanoo@gmail.com>
2025-10-13 02:27:06 +0200

    Edit DocumentEditor.jsx - add Chat- and Comments-components (rendered with DocumentForm)

--- src/document-components/DocumentEditor.jsx
+++ src/document-components/DocumentEditor.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDocumentContext } from './DocumentContext';
 import SavedDocuments from './SavedDocuments';
 import DocumentForm from './DocumentForm';
+import Chat from '../view-components/Chat';
+import Comments from '../view-components/Comments';
 
 /**
  * @component DocumentEditor
@@ -11,31 +13,66 @@ function DocumentEditor() {
     const {
         mode,
         switchToViewMode,
-        createDocument
+        createDocument,
+        chatDisplayed,
+        setChatDisplayed,
+        commentsDisplayed,
+        setCommentsDisplayed
     } = useDocumentContext();
 
-    return (
-        <div>
-            {/* Conditional rendering of the correct top button based on mode:*/}
 
-            <div className="top-button-container">
-                {mode === 'view' && (
-                    <button onClick={createDocument} className="top-button create-button">+ New Document</button>
+    const toggleChatVisibility = () => {
+        setChatDisplayed(chatDisplayed => !chatDisplayed);
+    };
+    
+    const toggleCommentsVisibility = () => {
+        setCommentsDisplayed(commentsDisplayed => !commentsDisplayed);
+    };
+
+    return (
+        <>
+            <div className="main-content">
+
+                {/* Conditional rendering of the correct top button based on mode: */}
+
+                <div className="top-button-container">
+                    {mode === 'view' && (
+                        <>
+                        <button onClick={createDocument} className="top-button back-button" type="button">
+                            Create New Document
+                        </button>
+                        </>
+                    )}
+                    {mode === 'update' && (
+                        <button onClick={switchToViewMode} className="top-button back-button" type="button">
+                            ← Back
+                        </button>
+                    )}
+                    <br />
+                </div>
+
+                {/* Conditional rendering of the correct Document Component based on mode: */}
+
+                {mode === 'view' ? (
+                    <SavedDocuments />
+                ) : (
+                mode === 'update' && (
+                    <>
+                    <DocumentForm key="document-quill-editor-form" />
+                    </>
+                )
                 )}
-                {(mode === 'create' || mode === 'update') && (
-                    <button onClick={switchToViewMode} className="top-button back-button" type="button">← Back</button>
-                )}
-                <br />
             </div>
 
-            {/* Conditional rendering of the correct Document Component based on mode:*/}
-
-            {mode === 'view' ? (
-                <SavedDocuments />
-            ) : (mode === 'create' || mode === 'update') && (
-                <DocumentForm />
+            {/* Chat and Comments accompanies DocumentForm in update-mode, but outside of the main-content on respective flanks. */}
+            
+            {mode === 'update' && (
+                <>
+                <Chat chatVisible={chatDisplayed} toggle={toggleChatVisibility} />
+                <Comments commentsVisible={commentsDisplayed} toggle={toggleCommentsVisibility} />
+                </>
             )}
-        </div>
+        </>
     );
 }
 
