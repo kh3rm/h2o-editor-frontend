4797201ea70a5e741690072ee69cb85614f4d149
h3rmano <h3rmanoo@gmail.com>
2025-10-16 04:51:31 +0200

    Add dynamic Header color effect (switching to/from code-edit-mode)

--- src/view-components/Header.jsx
+++ src/view-components/Header.jsx
@@ -1,9 +1,14 @@
 import logo from "../img/logo1.svg";
-import poolLadder from "../img/pool2.svg"
+import { useDocumentContext } from "../document-components/DocumentContext";
 
 function Header() {
+  const { mode } = useDocumentContext();
+
+  const headerDynamicColor = {
+    backgroundColor: mode === 'code-edit' ? '#e9baff' : '#f5f9fc',
+  };
   return (
-    <header>
+    <header style={headerDynamicColor}>
       <div className="header-logo">
         <img src={logo} alt="Logo" style={{ width: 49, height: 49 }} />
         <h1 className="header-title">docpool</h1>
