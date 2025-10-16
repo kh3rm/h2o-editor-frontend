6254833c6b524cf7de53808c373cca63ccdf685f
h3rmano <h3rmanoo@gmail.com>
2025-10-16 04:57:57 +0200

    Add monaco-setup.js to main.jsx (+ temporary disabling of StrictMode)

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '../monaco-setup.js';
 import 'quill/dist/quill.core.css';
 import './index.css'
 import './App.css'
@@ -7,7 +8,5 @@ import './App.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
