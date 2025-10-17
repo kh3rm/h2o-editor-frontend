e24f448f26099c7bcef6ca7729f94206ebbeec98
Olof Jönsson <epost.olof@gmail.com>
2025-10-17 18:46:30 +0200

    Add (empty) links to reset password and sign up

--- src/account-components/LoginForm.jsx
+++ src/account-components/LoginForm.jsx
@@ -15,42 +15,57 @@ function LoginForm() {
         auth.logIn(email, password);
     }
 
+    function goToResetPassword() {
+        // TODO
+    }
+
+    function goToSignup() {
+        // TODO
+    }
+
     return (
-        <form className="login-form" onSubmit={handleSubmit}>
-            <h3>Log in</h3>
+        <>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <h3>Log in to h<sub>2</sub>o docpool</h3>
 
-            <label htmlFor="email">Email</label>
-            <input
-                type="email"
-                name="email"
-                title="Enter the email of your account"
-                id="email"
-                onChange={e => setEmail(e.target.value)}
-                required
-            />
-
-            <label htmlFor="password">Password</label>
-            <div className="password-wrapper">
+                <label htmlFor="email">Email</label>
                 <input
-                    type={ show ? "text" : "password" }
-                    name="password"
-                    id="password"
-                    title="Enter the password of your acount"
-                    onChange={e => setPassword(e.target.value)}
+                    type="email"
+                    name="email"
+                    title="Enter the email of your account"
+                    id="email"
+                    onChange={e => setEmail(e.target.value)}
                     required
                 />
 
-                <span
-                    className="toggle-visibility"
-                    onClick={() => setShow(!show)}
-                >
-                    {show ? "🔓" : "🔒"}
-                </span>
-            </div>
+                <label htmlFor="password">Password</label>
+                <div className="password-wrapper">
+                    <input
+                        type={ show ? "text" : "password" }
+                        name="password"
+                        id="password"
+                        title="Enter the password of your acount"
+                        onChange={e => setPassword(e.target.value)}
+                        required
+                    />
+
+                    <span
+                        className="toggle-visibility"
+                        onClick={() => setShow(!show)}
+                    >
+                        {show ? "🔓" : "🔒"}
+                    </span>
+                </div>
 
 
-            <button type="submit" className="login-form-button">Log in</button>
-        </form>
+                <button type="submit" className="login-form-button">Log in</button>
+            </form>
+        
+            <p><span className="link" onClick={goToResetPassword}>Forgot your password?</span></p>
+
+            <p><span className="link" onClick={goToSignup}>Sign up to h<sub>2</sub>o docpool</span></p>
+
+        </>
     );    
 }
 
